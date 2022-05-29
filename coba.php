31703bedf38f6033e532c403c4ccb2a8f17ebee6
nevarida24 <nevarida24@gmail.com>
2022-05-29 19:34:35 +0700

    git

---
+++ coba.php
@@ -0,0 +1,15 @@
+<?php
+    $a = 100;
+    $b= 10;
+    $n= 6;
+    // menampilkan jumlah data ke
+    for($i = 1; $i <= $n; $i++){
+        //menghitung data ke
+        $un = $a+($i-1)*$b;
+        //menentukan bulan
+        $bulan = $i + $n;
+        //menampilkan hasil deret bilangan
+        echo "penjualan dibulan ke $i adalah ",($un);
+        echo "<br>";
+}
+?>
